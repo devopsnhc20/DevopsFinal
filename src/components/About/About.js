ecc476bdf684210289a36c947c7de0e962f767f5
devopsnhc20 <126224045+devopsnhc20@users.noreply.github.com>
2023-03-02 10:32:10 +0530

    Update About.js

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -17,7 +17,7 @@ function About() {
           Our mission is what drives us to do everything possible to expand
           human potential. We do that by creating groundbreaking sport
           innovations, by making our products more sustainably, by building a
-          creative and diverseacadcadc global team and by making a positive impact in
+          creative and diverse global team and by making a positive impact in
           communities where we live and work. Based in Beaverton, Oregon, NIKE,
           Inc. includes the Nike, Converse, and Jordan brands.
         </p>
